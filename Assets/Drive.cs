d0bf7fda2b1299f2a2e4a8939ccac70f911037e1
TejusWadbudhe <tejuswadbudhe06@gmail.com>
2020-03-19 11:07:42 +0530

    First commit

---
+++ Assets/Drive.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Drive : MonoBehaviour
+{
+    float speed = 1.0f;
+    float rotSpeed = 50.0f;
+    Animator anim;
+
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float translation = CrossPlatformInputManager.GetAxis("Vertical") * speed;
+        float rotation = CrossPlatformInputManager.GetAxis("Horizontal") * rotSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);  //for z movement
+        transform.Rotate(0, rotation, 0);    //for y movement
+
+        if (translation > 0)  //w is pressed
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetFloat("speed", 1.0f);
+        }
+        else if (translation < 0)   //s is pressed
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetFloat("speed", -1.0f);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("jump");
+        }
+
+    }
+}
---
+++ Assets/TouchToMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchToMove : MonoBehaviour
+{
+    public GameObject foodPrefab;
+    GameObject food;
+    Vector3 goalPos; //position of burger
+    float speed = 0.5f, accuracy = 0.25f, rotSpeed = 2f;
+    Animator anim;
+
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && food == null)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, 1000.0f)){
+                if (hit.collider.gameObject.tag == "ground")
+                {
+                    goalPos = hit.point;
+                    food = Instantiate(foodPrefab, goalPos, Quaternion.identity);
+                    Invoke("RemoveFood", 4.0f);
+                }
+            }
+        }
+    }
+
+    void RemoveFood()
+    {
+        Destroy(food);
+    }
+
+    void LateUpdate()
+    {
+        Vector3 lookAtGoal = new Vector3(goalPos.x, this.transform.position.y, goalPos.z);
+        Vector3 direction = llokAtGoal - this.transform.position;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+                                    Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+        if(Vector3.Distance(transform.position, lookAtGoal) > accuracy)
+        {
+            this.transform.translate(0, 0, speed * Time.deltaTime);
+            anim.SetBool("isRunning", true);
+            anim.SetFloat("speed", 1.0f);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+            if (food != null){
+                anim.SetBool("isEating", true);
+            }
+            if(food == null)
+            {
+                anim.SetBool("isEating", false);
+            }
+        }
+
+    }
+}
